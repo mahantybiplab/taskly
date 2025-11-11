4e0fdae70242ead957b58c148a9f494c38aa672f
biplab mahanty <bmahanty501@gmail.com>
2025-11-11 23:10:54 +0530

    Use icons for buttons , Install and configure expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
 import { StyleSheet, View } from "react-native";
-import { theme } from "./theme";
-import { ShoppingListItem } from "./components/ShoppingListItem";
+import { theme } from "../theme";
+import { ShoppingListItem } from "../components/ShoppingListItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" />
+      <ShoppingListItem name="Coffee" isCompleted />
+      <ShoppingListItem name="Sugar" isCompleted />
     </View>
   );
 }
