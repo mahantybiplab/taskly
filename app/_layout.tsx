f8b9f2425606a71853f30796e0423bba2e0fc920
biplab mahanty <bmahanty501@gmail.com>
2025-11-12 09:54:15 +0530

    Example of presentation modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,22 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping list" }} />
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          // animation: "fade_from_bottom",
+        }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{
+          title: "My idea",
+          presentation: "modal",
+          // animation: "fade_from_bottom",
+        }}
+      />
     </Stack>
   );
 }
